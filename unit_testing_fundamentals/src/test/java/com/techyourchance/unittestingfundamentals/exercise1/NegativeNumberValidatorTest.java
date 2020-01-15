74689df37d0080c6c1fa95acea66790607f6e749
Hady Salhab <hadysalhab1@gmail.com>
2020-01-16 00:31:13 +1100

    completing Exercise 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
+    private NegativeNumberValidator mNegativeNumberValidator;
+    @Before
+    public void setup(){
+        mNegativeNumberValidator = new NegativeNumberValidator();
+    }
+    @Test
+    public void whenGivenPositiveNumber_shouldReturnFalse(){
+        boolean result = mNegativeNumberValidator.isNegative(1);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void whenGiveNegativeNumber_shouldReturnTrue(){
+        boolean result = mNegativeNumberValidator.isNegative(-1);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void whenGivenZero_shouldReturnFalse(){
+        boolean result = mNegativeNumberValidator.isNegative(0);
+        Assert.assertFalse(result);
+    }
 
 }
