195fba87785cacbb2ab28e208a7028a4b2fd95dd
Hady Salhab <hadysalhab1@gmail.com>
2020-01-16 22:39:56 +1100

    Implementing Exercise#2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,31 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class StringDuplicatorTest {
+    private StringDuplicator mStringDuplicator;
+    @Before
+    public void setUp() throws Exception {
+        mStringDuplicator = new StringDuplicator();
+    }
+    @Test
+    public void duplicate_emptyString_shouldReturnEmptyString(){
+        String string = "";
+        String result = mStringDuplicator.duplicate(string);
+        Assert.assertEquals(string,result);
+    }
+    @Test
+    public void duplicate_singleCharacter_shouldReturnDoubleCharacter(){
+        String string = "a";
+        String result = mStringDuplicator.duplicate(string);
+        Assert.assertEquals(string+string,result);
+    }
+    @Test
+    public void duplicate_multipleCharacter_shouldRetunrItsDouble(){
+        String string="hady";
+        String result = mStringDuplicator.duplicate(string);
+        Assert.assertEquals(string+string,result);
+    }
 }
